5bca6abe29573f44ddb32ce487adf710ee8d50c5
eviKS23 <eks2733@rit.edu>
2025-06-03 09:12:33 -0400

    Wrote header and paragraph tags in header

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -4,8 +4,12 @@ const LoginPage = () => {
     return(
 
         <>
-            <h2 className="text-black"> about me test page</h2>
-
+            <h1 className="text-black"> About the Project</h1>
+            <p>
+            Lorem ipsum dolor sit amet consectetur. Eget pharetra accumsan donec euismod viverra aliquam. Nibh rhoncus velit volutpat arcu. Dui elementum neque lacus cursus lacus sem convallis enim. Ullamcorper dolor ultricies in mauris orci malesuada. Cursus commodo urna interdum eget pharetra. Nulla enim sit varius tempus tincidunt in in morbi malesuada. Nunc nunc volutpat sit quis ullamcorper eget turpis porta. Maecenas eget suspendisse sagittis volutpat non sed lorem. Vulputate at faucibus leo rhoncus. Ac ut adipiscing commodo elementum quis adipiscing scelerisque sed nulla. Malesuada nunc nec malesuada pretium vulputate faucibus leo pellentesque. Curabitur commodo nibh egestas pretium adipiscing rhoncus.
+            Fermentum adipiscing fusce id scelerisque pellentesque ipsum. Mattis sodales iaculis faucibus nam est quam. Vitae morbi tempus lectus elit odio. Justo varius nisi.
+            </p>
+            <img src="../assets/"></img>
         </>
     );
 }
