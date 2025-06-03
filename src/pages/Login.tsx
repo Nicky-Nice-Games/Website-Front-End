4280a6a6beecd799243492a76dd9cb4d29358040
AazeemVS <av7304@rit.edu>
2025-06-02 20:39:30 -0400

    Completed most of the login page
    Used a Shadcn log in component then edited the tailwind css using the RIT color pallete for a fun login page aesthetic

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,26 @@
+import { GalleryVerticalEnd } from "lucide-react"
+import { LoginForm } from "@/components/login-form"
+import { Trophy } from "lucide-react";
+
+
 const LoginPage = () => {
     return(
 
         <>
-            <h2 className="text-black"> checking router</h2>
-            <button> Enter email</button>
+            <div className="bg-[#000000] flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+                 <div className="bg-[#F76902] text-white flex size-12 items-center justify-center rounded-full">
+                    <Trophy className="size-8" />
+                </div>
+                <div className="flex w-full max-w-sm flex-col gap-6">
+                    <a href="#" className="flex items-center gap-2 self-center font-medium">
+                       
+                    <h1 className="text-white text-4xl">
+                        Name TBD
+                    </h1>
+                    </a>
+                    <LoginForm />
+                </div>
+            </div>
         </>
     );
 }
