ceb2f0dc100165b447f42517c15d2f8119fdf7bf
eviKS23 <eks2733@rit.edu>
2025-06-03 10:15:50 -0400

    Added image to about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,4 @@
-
+import photo from '../assets/Media/Images/placeholder.png';
 
 const LoginPage = () => {
     return(
@@ -9,7 +9,7 @@ const LoginPage = () => {
             Lorem ipsum dolor sit amet consectetur. Eget pharetra accumsan donec euismod viverra aliquam. Nibh rhoncus velit volutpat arcu. Dui elementum neque lacus cursus lacus sem convallis enim. Ullamcorper dolor ultricies in mauris orci malesuada. Cursus commodo urna interdum eget pharetra. Nulla enim sit varius tempus tincidunt in in morbi malesuada. Nunc nunc volutpat sit quis ullamcorper eget turpis porta. Maecenas eget suspendisse sagittis volutpat non sed lorem. Vulputate at faucibus leo rhoncus. Ac ut adipiscing commodo elementum quis adipiscing scelerisque sed nulla. Malesuada nunc nec malesuada pretium vulputate faucibus leo pellentesque. Curabitur commodo nibh egestas pretium adipiscing rhoncus.
             Fermentum adipiscing fusce id scelerisque pellentesque ipsum. Mattis sodales iaculis faucibus nam est quam. Vitae morbi tempus lectus elit odio. Justo varius nisi.
             </p>
-            <img src="../assets/"></img>
+            <img src={photo} alt="Photo of the team" />
         </>
     );
 }
