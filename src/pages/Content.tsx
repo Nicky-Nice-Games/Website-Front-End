e5db73c6343a7d2f5a026658d6c90c1c7a9aca5c
That-Bot-Tess <90156876+That-Bot-Tess@users.noreply.github.com>
2025-06-04 16:09:34 -0400

    expandable cards in tracks page

--- src/pages/Content.tsx
+++ src/pages/Content.tsx
@@ -1,4 +1,7 @@
 import { useNavigate } from "react-router-dom";
+import { useEffect, useId, useRef, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { useOutsideClick } from "@/hooks/use-outside-click";
 
 //import ItemsPage from './Items';
 // import CharactersPage from './Characters';
@@ -58,97 +61,97 @@ const ItemsPage = () => {
     return (
         <>
             <ContentNavigator />
-           <div className="px-8">
-  <h2 className="text-black text-3xl ml-8">Items Page</h2>
+            <div className="px-8">
+                <h2 className="text-black text-3xl ml-8">Items Page</h2>
 
-  <div className="grid grid-cols-6 gap-4 p-8 mx-auto max-w-6xl">
-    <div className="bg-gray-300 border-2 border-gray-600 rounded-xl h-24 w-24 flex items-center justify-center">
-      <img
-        src={PlaceholderImg}
-        alt="Item placeholder image"
-        className="h-16 w-16"
-      />
-    </div>
+                <div className="grid grid-cols-6 gap-4 p-8 mx-auto max-w-6xl">
+                    <div className="bg-gray-300 border-2 border-gray-600 rounded-xl h-24 w-24 flex items-center justify-center">
+                        <img
+                            src={PlaceholderImg}
+                            alt="Item placeholder image"
+                            className="h-16 w-16"
+                        />
+                    </div>
 
-    <div className="bg-gray-300 border-2 border-gray-600 rounded-xl h-24 w-24 flex items-center justify-center">
-      <img
-        src={PlaceholderImg}
-        alt="Item placeholder image"
-        className="h-16 w-16"
-      />
-    </div>
-    <div className="bg-gray-300 border-2 border-gray-600 rounded-xl h-24 w-24 flex items-center justify-center">
-      <img
-        src={PlaceholderImg}
-        alt="Item placeholder image"
-        className="h-16 w-16"
-      />
-    </div>
-    <div className="bg-gray-300 border-2 border-gray-600 rounded-xl h-24 w-24 flex items-center justify-center">
-      <img
-        src={PlaceholderImg}
-        alt="Item placeholder image"
-        className="h-16 w-16"
-      />
-    </div>
-    <div className="bg-gray-300 border-2 border-gray-600 rounded-xl h-24 w-24 flex items-center justify-center">
-      <img
-        src={PlaceholderImg}
-        alt="Item placeholder image"
-        className="h-16 w-16"
-      />
-    </div>
-    <div className="bg-gray-300 border-2 border-gray-600 rounded-xl h-24 w-24 flex items-center justify-center">
-      <img
-        src={PlaceholderImg}
-        alt="Item placeholder image"
-        className="h-16 w-16"
-      />
-    </div>
-    <div className="bg-gray-300 border-2 border-gray-600 rounded-xl h-24 w-24 flex items-center justify-center">
-      <img
-        src={PlaceholderImg}
-        alt="Item placeholder image"
-        className="h-16 w-16"
-      />
-    </div>
-    <div className="bg-gray-300 border-2 border-gray-600 rounded-xl h-24 w-24 flex items-center justify-center">
-      <img
-        src={PlaceholderImg}
-        alt="Item placeholder image"
-        className="h-16 w-16"
-      />
-    </div>
-    <div className="bg-gray-300 border-2 border-gray-600 rounded-xl h-24 w-24 flex items-center justify-center">
-      <img
-        src={PlaceholderImg}
-        alt="Item placeholder image"
-        className="h-16 w-16"
-      />
-    </div>
-    <div className="bg-gray-300 border-2 border-gray-600 rounded-xl h-24 w-24 flex items-center justify-center">
-      <img
-        src={PlaceholderImg}
-        alt="Item placeholder image"
-        className="h-16 w-16"
-      />
-    </div>
-    <div className="bg-gray-300 border-2 border-gray-600 rounded-xl h-24 w-24 flex items-center justify-center">
-      <img
-        src={PlaceholderImg}
-        alt="Item placeholder image"
-        className="h-16 w-16"
-      />
-    </div>
-    <div className="bg-gray-300 border-2 border-gray-600 rounded-xl h-24 w-24 flex items-center justify-center">
-      <img
-        src={PlaceholderImg}
-        alt="Item placeholder image"
-        className="h-16 w-16"
-      />
-    </div>
-  </div>
-</div>
+                    <div className="bg-gray-300 border-2 border-gray-600 rounded-xl h-24 w-24 flex items-center justify-center">
+                        <img
+                            src={PlaceholderImg}
+                            alt="Item placeholder image"
+                            className="h-16 w-16"
+                        />
+                    </div>
+                    <div className="bg-gray-300 border-2 border-gray-600 rounded-xl h-24 w-24 flex items-center justify-center">
+                        <img
+                            src={PlaceholderImg}
+                            alt="Item placeholder image"
+                            className="h-16 w-16"
+                        />
+                    </div>
+                    <div className="bg-gray-300 border-2 border-gray-600 rounded-xl h-24 w-24 flex items-center justify-center">
+                        <img
+                            src={PlaceholderImg}
+                            alt="Item placeholder image"
+                            className="h-16 w-16"
+                        />
+                    </div>
+                    <div className="bg-gray-300 border-2 border-gray-600 rounded-xl h-24 w-24 flex items-center justify-center">
+                        <img
+                            src={PlaceholderImg}
+                            alt="Item placeholder image"
+                            className="h-16 w-16"
+                        />
+                    </div>
+                    <div className="bg-gray-300 border-2 border-gray-600 rounded-xl h-24 w-24 flex items-center justify-center">
+                        <img
+                            src={PlaceholderImg}
+                            alt="Item placeholder image"
+                            className="h-16 w-16"
+                        />
+                    </div>
+                    <div className="bg-gray-300 border-2 border-gray-600 rounded-xl h-24 w-24 flex items-center justify-center">
+                        <img
+                            src={PlaceholderImg}
+                            alt="Item placeholder image"
+                            className="h-16 w-16"
+                        />
+                    </div>
+                    <div className="bg-gray-300 border-2 border-gray-600 rounded-xl h-24 w-24 flex items-center justify-center">
+                        <img
+                            src={PlaceholderImg}
+                            alt="Item placeholder image"
+                            className="h-16 w-16"
+                        />
+                    </div>
+                    <div className="bg-gray-300 border-2 border-gray-600 rounded-xl h-24 w-24 flex items-center justify-center">
+                        <img
+                            src={PlaceholderImg}
+                            alt="Item placeholder image"
+                            className="h-16 w-16"
+                        />
+                    </div>
+                    <div className="bg-gray-300 border-2 border-gray-600 rounded-xl h-24 w-24 flex items-center justify-center">
+                        <img
+                            src={PlaceholderImg}
+                            alt="Item placeholder image"
+                            className="h-16 w-16"
+                        />
+                    </div>
+                    <div className="bg-gray-300 border-2 border-gray-600 rounded-xl h-24 w-24 flex items-center justify-center">
+                        <img
+                            src={PlaceholderImg}
+                            alt="Item placeholder image"
+                            className="h-16 w-16"
+                        />
+                    </div>
+                    <div className="bg-gray-300 border-2 border-gray-600 rounded-xl h-24 w-24 flex items-center justify-center">
+                        <img
+                            src={PlaceholderImg}
+                            alt="Item placeholder image"
+                            className="h-16 w-16"
+                        />
+                    </div>
+                </div>
+            </div>
 
         </>
     );
@@ -163,23 +166,207 @@ const CharactersPage = () => {
     );
 }
 
+interface Track {
+    imgUrl: string;
+    name: string;
+    description: string;
+}
+
+const tracks: Array<Track> = [
+    {
+        imgUrl: "images/placeholder.PNG",
+        name: "Sample Track",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati atque aperiam quo, consectetur architecto officia aliquid ea corrupti asperiores, ut quos. Excepturi atque quae minima. Possimus nemo eaque similique fugiat."
+    },
+    {
+        imgUrl: "images/placeholder.PNG",
+        name: "Sample Track",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati atque aperiam quo, consectetur architecto officia aliquid ea corrupti asperiores, ut quos. Excepturi atque quae minima. Possimus nemo eaque similique fugiat."
+    },
+    {
+        imgUrl: "images/placeholder.PNG",
+        name: "Sample Track",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati atque aperiam quo, consectetur architecto officia aliquid ea corrupti asperiores, ut quos. Excepturi atque quae minima. Possimus nemo eaque similique fugiat."
+    }
+]
+
 const TracksPage = () => {
+    const [active, setActive] = useState<(typeof tracks)[number] | boolean | null>(
+        null
+    );
+    const id = useId();
+    const ref = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        function onKeyDown(event: KeyboardEvent) {
+            if (event.key === "Escape") {
+                setActive(false);
+            }
+        }
+
+        if (active && typeof active === "object") {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "auto";
+        }
+
+        window.addEventListener("keydown", onKeyDown);
+        return () => window.removeEventListener("keydown", onKeyDown);
+    }, [active]);
+
+    useOutsideClick(ref, () => setActive(null));
+
     return (
         <>
             <ContentNavigator />
-            <h2 className="text-white text-center text-xl rounded-lg bg-[#7C878E] relative top-4 z-10 w-50 m-auto">Tracks</h2>
-            <Carousel className="text-center border-solid border-slate-400 border-8 rounded-3xl w-180 m-auto">
-                <CarouselContent>
-                    <CarouselItem><img src="images/placeholder.PNG" className="rounded-xl h-80 m-auto w-full h-full"/></CarouselItem>
-                    <CarouselItem><img src="images/placeholder.PNG" className="rounded-xl h-80 m-auto w-full h-full"/></CarouselItem>
-                    <CarouselItem><img src="images/placeholder.PNG" className="rounded-xl h-80 m-auto w-full h-full"/></CarouselItem>
-                </CarouselContent>
-                <CarouselPrevious />
-                <CarouselNext />
-            </Carousel>
+            <AnimatePresence>
+                {active && typeof active === "object" && (
+                    <motion.div
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        className="fixed inset-0 bg-black/20 h-full w-full z-41"
+                    />
+                )}
+            </AnimatePresence>
+            <AnimatePresence>
+                {active && typeof active === "object" ? (
+                    <div className="fixed inset-0  grid place-items-center z-[100]">
+                        <motion.button
+                            key={`button-${active.name}-${id}`}
+                            layout
+                            initial={{
+                                opacity: 0,
+                            }}
+                            animate={{
+                                opacity: 1,
+                            }}
+                            exit={{
+                                opacity: 0,
+                                transition: {
+                                    duration: 0.05,
+                                },
+                            }}
+                            className="flex absolute top-2 right-2 lg:hidden items-center justify-center bg-white rounded-full h-6 w-6"
+                            onClick={() => setActive(null)}
+                        >
+                            <CloseIcon />
+                        </motion.button>
+                        <motion.div
+                            layoutId={`track-${active.name}-${id}`}
+                            ref={ref}
+                            className="w-full max-w-[500px]  h-full md:h-fit md:max-h-[90%]  flex flex-col bg-white dark:bg-neutral-900 sm:rounded-3xl overflow-hidden"
+                        >
+                            <motion.div layoutId={`image-${active.name}-${id}`}>
+                                <img
+                                    width={200}
+                                    height={200}
+                                    src={active.imgUrl}
+                                    alt={active.name}
+                                    className="w-full h-80 lg:h-80 sm:rounded-tr-lg sm:rounded-tl-lg object-cover object-top"
+                                />
+                            </motion.div>
+
+                            <div>
+                                <div className="flex justify-between items-start p-4">
+                                    <div className="">
+                                        <motion.h3
+                                            layoutId={`title-${active.name}-${id}`}
+                                            className="font-medium text-neutral-700 dark:text-neutral-200 text-base"
+                                        >
+                                            {active.name}
+                                        </motion.h3>
+                                        <motion.p
+                                            layoutId={`description-${active.description}-${id}`}
+                                            className="text-neutral-600 dark:text-neutral-400 text-base"
+                                        >
+                                            {active.description}
+                                        </motion.p>
+                                    </div>
+                                </div>
+                                <div className="pt-4 relative px-4">
+                                    <motion.div
+                                        layout
+                                        initial={{ opacity: 0 }}
+                                        animate={{ opacity: 1 }}
+                                        exit={{ opacity: 0 }}
+                                        className="text-neutral-600 text-xs md:text-sm lg:text-base h-40 md:h-fit pb-10 flex flex-col items-start gap-4 overflow-auto dark:text-neutral-400 [mask:linear-gradient(to_bottom,white,white,transparent)] [scrollbar-width:none] [-ms-overflow-style:none] [-webkit-overflow-scrolling:touch]"
+                                    >
+                                    </motion.div>
+                                </div>
+                            </div>
+                        </motion.div>
+                    </div>
+                ) : null}
+            </AnimatePresence>
+            <div className="mb-10">
+                <h2 className="text-white text-center text-xl rounded-lg bg-[#7C878E] relative top-4 z-10 w-50 m-auto">Tracks</h2>
+                <Carousel className="text-center border-solid border-slate-400 border-8 rounded-3xl w-180 m-auto">
+                    <CarouselContent>
+                        {tracks.map(track => {
+                            return <CarouselItem>
+                                    <motion.div
+                                        layoutId={`track-${track.name}-${id}`}
+                                        key={track.name}
+                                        onClick={() => setActive(track)}
+                                        className="p-4 flex flex-col  hover:bg-neutral-50 dark:hover:bg-neutral-800 rounded-xl cursor-pointer"
+                                    >
+                                        <div className="flex gap-4 flex-col w-full h-full">
+                                            <motion.div layoutId={`image-${track.name}-${id}`}>
+                                                <img
+                                                    width={100}
+                                                    height={200}
+                                                    src={track.imgUrl}
+                                                    alt={track.name}
+                                                    className="h-full w-full  rounded-lg object-cover object-top"
+                                                />
+                                            </motion.div>
+                                        </div>
+                                    </motion.div>
+                            </CarouselItem>
+                        })}
+                    </CarouselContent>
+                    <CarouselPrevious />
+                    <CarouselNext />
+                </Carousel>
+            </div>
+
         </>
     );
 }
+
+const CloseIcon = () => {
+    return (
+        <motion.svg
+            initial={{
+                opacity: 0,
+            }}
+            animate={{
+                opacity: 1,
+            }}
+            exit={{
+                opacity: 0,
+                transition: {
+                    duration: 0.05,
+                },
+            }}
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="h-4 w-4 text-black"
+        >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M18 6l-12 12" />
+            <path d="M6 6l12 12" />
+        </motion.svg>
+    );
+};
 
 export {
     ContentPage,
