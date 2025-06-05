780c38588418d0a86b2b052de9d92d5c98ac46b3
That-Bot-Tess <90156876+That-Bot-Tess@users.noreply.github.com>
2025-06-05 11:23:57 -0400

    track popup layout adjusted

--- src/pages/Content.tsx
+++ src/pages/Content.tsx
@@ -231,7 +231,7 @@ const TracksPage = () => {
             </AnimatePresence>
             <AnimatePresence>
                 {active && typeof active === "object" ? (
-                    <div className="fixed inset-0  grid place-items-center z-[100]">
+                    <div className="fixed inset-0 grid place-items-center z-[100]">
                         <motion.button
                             key={`button-${active.name}-${id}`}
                             layout
@@ -255,46 +255,35 @@ const TracksPage = () => {
                         <motion.div
                             layoutId={`track-${active.name}-${id}`}
                             ref={ref}
-                            className="w-full max-w-[500px]  h-full md:h-fit md:max-h-[90%]  flex flex-col bg-white dark:bg-neutral-900 sm:rounded-3xl overflow-hidden"
+                            className="h-full md:h-fit md:max-h-[90%] flex flex-row bg-white dark:bg-neutral-900 sm:rounded-3xl overflow-hidden"
                         >
-                            <motion.div layoutId={`image-${active.name}-${id}`}>
-                                <img
-                                    width={200}
-                                    height={200}
-                                    src={active.imgUrl}
-                                    alt={active.name}
-                                    className="w-full h-80 lg:h-80 sm:rounded-tr-lg sm:rounded-tl-lg object-cover object-top"
-                                />
-                            </motion.div>
-
                             <div>
                                 <div className="flex justify-between items-start p-4">
                                     <div className="">
                                         <motion.h3
                                             layoutId={`title-${active.name}-${id}`}
-                                            className="font-medium text-neutral-700 dark:text-neutral-200 text-base"
+                                            className="font-black text-center text-2xl text-neutral-700 dark:text-neutral-200 mb-5"
                                         >
                                             {active.name}
                                         </motion.h3>
                                         <motion.p
                                             layoutId={`description-${active.description}-${id}`}
-                                            className="text-neutral-600 dark:text-neutral-400 text-base"
+                                            className="text-neutral-600 dark:text-neutral-400 text-base max-h-60 overflow-y-scroll"
                                         >
                                             {active.description}
                                         </motion.p>
                                     </div>
                                 </div>
-                                <div className="pt-4 relative px-4">
-                                    <motion.div
-                                        layout
-                                        initial={{ opacity: 0 }}
-                                        animate={{ opacity: 1 }}
-                                        exit={{ opacity: 0 }}
-                                        className="text-neutral-600 text-xs md:text-sm lg:text-base h-40 md:h-fit pb-10 flex flex-col items-start gap-4 overflow-auto dark:text-neutral-400 [mask:linear-gradient(to_bottom,white,white,transparent)] [scrollbar-width:none] [-ms-overflow-style:none] [-webkit-overflow-scrolling:touch]"
-                                    >
-                                    </motion.div>
-                                </div>
                             </div>
+                            <motion.div layoutId={`image-${active.name}-${id}`} className="min-w-4/10">
+                                <img
+                                    src={active.imgUrl}
+                                    alt={active.name}
+                                    className="w-full sm:rounded-tr-lg sm:rounded-tl-lg object-cover object-top"
+                                />
+                            </motion.div>
+
+                            
                         </motion.div>
                     </div>
                 ) : null}
