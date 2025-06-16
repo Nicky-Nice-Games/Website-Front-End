5e489466c4517d314c478e7895145dbad3dcc938
That-Bot-Tess <90156876+That-Bot-Tess@users.noreply.github.com>
2025-06-16 16:07:45 -0400

    auth0 removed

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Auth0Provider
-    domain="dev-my7w0xyuzxg0t0db.us.auth0.com"
-    clientId="sujDzfekNdyUySR7C2PoiZe7SnBwSUxO"
-    authorizationParams={{
-      redirect_uri: window.location.origin
-    }}
-    >
       <App />
-    </Auth0Provider>
   </StrictMode>
 )
