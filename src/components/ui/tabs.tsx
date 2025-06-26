e2ca1b9142a75a6cf3b599a055017179dd0c0248
Ztw4322 <ztw4322@rit.edu>
2025-06-26 16:19:31 -0400

    Update tabs.tsx

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -40,8 +40,14 @@ function TabsTrigger({
     <TabsPrimitive.Trigger
       data-slot="tabs-trigger"
       className={cn(
-      " bg-orange-500 data-[state=active]:text-yellow-400 data-[state=hover]:bg-yellow-500 drop-shadow-[0_0_1px_#facc15] outline outline-2 outline-black focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring dark:data-[state=active]:border-input dark:data-[state=active]:bg-input/30 text-foreground dark:text-muted-foreground inline-flex h-[calc(100%-1px)] flex-1 items-center justify-center gap-1.5 rounded-md border border-transparent px-2 py-1 text-sm font-medium whitespace-nowrap transition-[color,box-shadow,background-color] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 md:text-2xl bebas p-[10px]",
-        className
+      `bg-orange-500 data-[state=active]:text-yellow-400 data-[state=hover]:bg-yellow-500 drop-shadow-[0_0_1px_#facc15]
+       outline outline-2 outline-black focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring 
+       dark:data-[state=active]:border-input dark:data-[state=active]:bg-input/30 text-foreground dark:text-muted-foreground
+        inline-flex h-[calc(100%-1px)] flex-1 items-center justify-center gap-1.5 rounded-md border border-transparent px-2 py-1
+         text-sm font-medium whitespace-nowrap transition-[color,box-shadow,background-color] focus-visible:ring-[3px] 
+         focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:shadow-sm
+          [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 md:text-2xl bebas p-[10px]`,
+      className
       )}
       {...props}
     />
