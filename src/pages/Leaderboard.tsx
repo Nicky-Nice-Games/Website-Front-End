b20e61e2ae72673b280fb9179a981f1feeba3a8b
That-Bot-Tess <90156876+That-Bot-Tess@users.noreply.github.com>
2025-06-30 14:12:41 -0400

    map select added

--- src/pages/Leaderboard.tsx
+++ src/pages/Leaderboard.tsx
@@ -3,6 +3,61 @@ import { columns } from "@/components/leaderboard/columns"
 import { DataTable } from "@/components/leaderboard/data-table"
 
 const LeaderboardPage = () => {
+    const [mapId, setMapId] = useState(0);
+
+    useEffect(()=> {}, [mapId]);
+
+    if (mapId < 1) return <div className="text-center">
+        <h1 className="bebas text-black italic mx-10 mt-8 text-5xl">Leaderboard</h1>
+        <h2 className="text-xl mb-8 font-semibold">Click a map to view its leaderboard</h2>
+        <MapSelect setMapId={setMapId} />
+    </div>
+
+    return <div className="text-center max-w-8/10 m-auto">
+        <button onClick={() => setMapId(0)} className="absolute left-[10%] top-[11.5vh] text-xl font-bold hover:text-orange-500">
+            Back
+        </button>
+        <h1 className="bebas text-black italic m-10 text-5xl">Leaderboard</h1>
+        <LeaderboardTable mapId={mapId} />
+    </div>
+}
+
+const MapSelect = ({ setMapId }: {setMapId: Function}) => {
+    interface Track{
+        name: string;
+        imgLink: string;
+    }
+    const tracks: Track[] = [
+        {
+            name: "RIT Outer Loop",
+            imgLink: "images/content-assets/OuterLoop.png"
+        },
+        {
+            name: "RIT Dorm Room",
+            imgLink: "images/placeholder.PNG"
+        },
+        {
+            name: "Golisano",
+            imgLink: "images/placeholder.PNG"
+        },
+        {
+            name: "Brick Road",
+            imgLink: "images/placeholder.PNG"
+        },
+    ]
+    return <div className="flex flex-col md:flex-row flex-wrap items-center md:justify-between">
+        {tracks.map(t => {
+            return <div className="md:w-[45%]">
+                <h1 className="text-lg">{t.name}</h1>
+                <button onClick={() => setMapId(tracks.indexOf(t) + 1)} className="hover:brightness-125">
+                    <img src={t.imgLink} className="object-fill h-[30vh]"/>
+                </button>
+            </div>
+        })}
+    </div>
+}
+
+const LeaderboardTable = ({ mapId }: {mapId: number}) => {
     const [leaderboardData, setLeaderboardData] = useState([]);
 
     useEffect(() => {
@@ -20,18 +75,8 @@ const LeaderboardPage = () => {
         getLeaderboardData();
     }, [])
     
-    if (leaderboardData.length == 0) return(
-        <div className="text-center">
-                <h1 className="bebas text-black italic m-10 text-5xl">Leaderboard</h1>
-            <p>No data found!</p>
-        </div>
-    );
+    if (leaderboardData.length == 0) return<p>No data found!</p>;
 
-    return (
-        <div className="text-center max-w-8/10 m-auto">
-            <h1 className="bebas text-black italic m-10 text-5xl"> Leaderboard</h1>
-                <DataTable columns={columns} data={leaderboardData} />
-        </div>
-    )
+    return <DataTable columns={columns} data={leaderboardData} />;
 }
 export default LeaderboardPage
