66b4811751fa4f81c9464708ac6a027f91a86406
eviKS23 <eks2733@rit.edu>
2025-06-30 15:45:15 -0400

    added fonts to headers

--- src/pages/Process.tsx
+++ src/pages/Process.tsx
@@ -43,7 +43,7 @@ const ProcessPage = () => {
             </div>
         </div>
         <div>
-            <h1 className="font-black text-4xl m-4 mx-0 mb-[2rem]">Designing our characters</h1>
+            <h1 className="font-black text-[60px] m-4 mx-0 mt-[2rem] graffiti mb-[2rem]">Designing our characters</h1>
             <p>
                 Lorem ipsum dolor sit amet consectetur. Eget pharetra accumsan donec euismod viverra aliquam. Nibh rhoncus velit volutpat arcu. Dui elementum neque lacus cursus lacus sem convallis enim. Ullamcorper dolor ultricies in mauris orci malesuada. Cursus commodo urna interdum eget pharetra. Nulla enim sit varius tempus tincidunt in in morbi malesuada. Nunc nunc volutpat sit quis ullamcorper eget turpis porta. Maecenas eget suspendisse sagittis volutpat non sed lorem. Vulputate at faucibus leo rhoncus. Ac ut adipiscing commodo elementum quis adipiscing scelerisque sed nulla. Malesuada nunc nec malesuada pretium vulputate faucibus leo pellentesque. Curabitur commodo nibh egestas pretium adipiscing rhoncus.
             </p>
@@ -69,7 +69,7 @@ const ProcessPage = () => {
             </div>
         </div>
         
-        <h1 className="font-black text-[60px] m-4 mx-0 mt-[2rem] graffiti mb-[2rem]">Building our Tracks</h1>
+        <h1 className="font-black text-[50px] m-4 mx-0 mt-[2rem] bebas mb-[2rem]">Building our Tracks</h1>
 
         <div className="flex flex-row flex-wrap w-full justify-center">
             <Tabs defaultValue="track1">
