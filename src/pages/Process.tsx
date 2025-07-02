1ca4baf2c1e7ea8bf8ec09d109731a74ba09f4b3
eviKS23 <eks2733@rit.edu>
2025-07-02 16:23:51 -0400

    added pop ups to process page

--- src/pages/Process.tsx
+++ src/pages/Process.tsx
@@ -1,8 +1,10 @@
 import { useNavigate } from "react-router-dom"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-
+import { useState } from 'react';
+import { CharactersProcess } from "@/components/characters-process";
 const ProcessPage = () => {
     const photo:string = 'images/placeholder.PNG';
+    const [isVisible, setIsVisible] = useState(false);
 
     type PopUpProps = {
         name:string
@@ -12,13 +14,25 @@ const ProcessPage = () => {
         designSheetCap: string;
     };
 
-    function PopUp({ name, concepts, conceptsCap, designSheet, designSheetCap }: PopUpProps) {
+    function PopUp({ name, concepts, conceptsCap }: PopUpProps) {
+        const [isVisible, setIsVisible] = useState(true);
+
+        if (!isVisible) return null;
+
         return (
-            <div className="absolute top-[4rem] left-[4rem] w-[60%]">
+            <div className="fixed top-[25%] left-[20%] w-[60%] z-4 bg-[#e2e8f0]">
+                <div className="absolute top-[1rem] right-[1rem] cursor-pointer" onClick={() => setIsVisible(false)}>
+                    <svg width="30" height="30" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M12.8536 2.85355C13.0488 2.65829 13.0488 2.34171 12.8536 2.14645C12.6583 1.95118 12.3417 1.95118 12.1464 2.14645L7.5 6.79289L2.85355 2.14645C2.65829 1.95118 2.34171 1.95118 2.14645 2.14645C1.95118 2.34171 1.95118 2.65829 2.14645 2.85355L6.79289 7.5L2.14645 12.1464C1.95118 12.3417 1.95118 12.6583 2.14645 12.8536C2.34171 13.0488 2.65829 13.0488 2.85355 12.8536L7.5 8.20711L12.1464 12.8536C12.3417 13.0488 12.6583 13.0488 12.8536 12.8536C13.0488 12.6583 13.0488 12.3417 12.8536 12.1464L8.20711 7.5L12.8536 2.85355Z" 
+                            fill="currentColor" 
+                            fillRule="evenodd" 
+                            clipRule="evenodd" />
+                    </svg>
+                </div>
                 <h1>{name}</h1>
                 <div className="flex flex-wrap">
                     <div className="flex flex-column">
-                        <img src="concepts"></img>
+                        <img src={concepts} alt="Concept" />
                         <p>{conceptsCap}</p>
                     </div>
                 </div>
@@ -29,7 +43,7 @@ const ProcessPage = () => {
     return(<>
     <main className="ml-[2rem] mr-[2rem]">
         <div>
-            <h1 className="font-black text-4xl m-4 mx-0 mb-[2rem]"> About the Project</h1>
+            <h1 className="text-[60px] painterz m-4 mx-0 mb-[2rem]"> About the Project</h1>
             <div className='flex md:flex-row flex-col items-stretch'>
                 <p>
                     Lorem ipsum dolor sit amet consectetur. Eget pharetra accumsan donec euismod viverra aliquam. Nibh rhoncus velit volutpat arcu. Dui elementum neque lacus cursus lacus sem convallis enim. Ullamcorper dolor ultricies in mauris orci malesuada. Cursus commodo urna interdum eget pharetra. Nulla enim sit varius tempus tincidunt in in morbi malesuada. Nunc nunc volutpat sit quis ullamcorper eget turpis porta. Maecenas eget suspendisse sagittis volutpat non sed lorem. Vulputate at faucibus leo rhoncus. Ac ut adipiscing commodo elementum quis adipiscing scelerisque sed nulla. Malesuada nunc nec malesuada pretium vulputate faucibus leo pellentesque. Curabitur commodo nibh egestas pretium adipiscing rhoncus.
@@ -43,7 +57,7 @@ const ProcessPage = () => {
         </div>
 
         <div>
-            <h1 className="font-black text-4xl m-4 mx-0 mb-[2rem]">Designing the Game</h1>
+            <h1 className="text-[60px] painterz m-4 mx-0 mb-[2rem]">Designing the Game</h1>
             <div className='flex md:flex-row flex-col items-stretch'>
                 <img src={photo} alt="Picture of RIT"
                     className='mr-[2rem] mb-[2rem] md:mt-0 self-center md:self-end 
@@ -65,33 +79,17 @@ const ProcessPage = () => {
             </div>
         </div>
         <div>
-            <h1 className="font-black text-[60px] m-4 mx-0 mt-[2rem] graffiti mb-[2rem]">Designing our characters</h1>
+            <h1 className="text-[60px] mx-0 m-4 mt-[2rem] painterz mb-[2rem]">
+                Designing our characters
+            </h1>
             <p>
                 Lorem ipsum dolor sit amet consectetur. Eget pharetra accumsan donec euismod viverra aliquam. Nibh rhoncus velit volutpat arcu. Dui elementum neque lacus cursus lacus sem convallis enim. Ullamcorper dolor ultricies in mauris orci malesuada. Cursus commodo urna interdum eget pharetra. Nulla enim sit varius tempus tincidunt in in morbi malesuada. Nunc nunc volutpat sit quis ullamcorper eget turpis porta. Maecenas eget suspendisse sagittis volutpat non sed lorem. Vulputate at faucibus leo rhoncus. Ac ut adipiscing commodo elementum quis adipiscing scelerisque sed nulla. Malesuada nunc nec malesuada pretium vulputate faucibus leo pellentesque. Curabitur commodo nibh egestas pretium adipiscing rhoncus.
             </p>
-            <div className="mt-[2rem] flex flex-row flex-wrap w-full justify-center">
-                <img src={photo} alt="Picture of RIT"
-                    className='m-[1rem] md:mt-0 self-center md:self-end md:w-1/4 w-[100%]'     
-                />
-                <img src={photo} alt="Picture of RIT"
-                    className='m-[1rem] md:mt-0 self-center md:self-end md:w-1/4 w-[100%]'     
-                />
-                <img src={photo} alt="Picture of RIT"
-                    className='m-[1rem] md:mt-0 self-center md:self-end md:w-1/4 w-[100%]'     
-                />
-                <img src={photo} alt="Picture of RIT"
-                    className='m-[1rem] md:mt-0 self-center md:self-end md:w-1/4 w-[100%]'     
-                />
-                <img src={photo} alt="Picture of RIT"
-                    className='m-[1rem] md:mt-0 self-center md:self-end md:w-1/4 w-[100%]'     
-                />
-                <img src={photo} alt="Picture of RIT"
-                    className='m-[1rem] md:mt-0 self-center md:self-end md:w-1/4 w-[100%]'     
-                />
-            </div>
+    
+            <CharactersProcess></CharactersProcess>
         </div>
         
-        <h1 className="font-black text-[50px] m-4 mx-0 mt-[2rem] bebas mb-[2rem]">Building our Tracks</h1>
+        <h1 className="text-[60px] m-4 mx-0 mt-[2rem] painterz mb-[2rem]">Building our Tracks</h1>
 
         <div className="flex flex-row flex-wrap w-full justify-center">
             <Tabs defaultValue="track1">
