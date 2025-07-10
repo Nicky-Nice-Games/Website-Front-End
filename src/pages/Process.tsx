11ccfda1576722a78d252f525655d5b3e856551c
That-Bot-Tess <90156876+That-Bot-Tess@users.noreply.github.com>
2025-07-10 13:27:42 -0400

    process page updated

--- src/pages/Process.tsx
+++ src/pages/Process.tsx
@@ -9,9 +9,12 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import ArrowButton from "@/components/ui/arrow-button";
+import { useNavigate } from "react-router-dom";
 
 const ProcessPage = () => {
   const photo: string = "images/placeholder/placeholder.PNG";
+  const navigate = useNavigate();
 
   return (
     <>
@@ -38,11 +41,18 @@ const ProcessPage = () => {
               ipsum. Mattis sodales iaculis faucibus nam est quam. Vitae morbi
               tempus lectus elit odio. Justo varius nisi.
             </p>
-            <img
-              src={photo}
-              alt="Photo of the team"
-              className="ml-[2rem] md:mt-0 self-center md:self-end md:w-[400px] w-[100%] h-[300px]"
-            />
+            <div className="md:min-w-[400px] md:w-550">
+              <img
+                src={photo}
+                alt="Photo of the team"
+                className="md:ml-[2rem] md:mt-0 self-center md:self-end md:pr-4 w-[100%] h-[300px]"
+              />
+              <ArrowButton
+                caption="Meet the team!"
+                clickAction={() => navigate("/aboutUs")}
+                className="absolute right-5 font-semibold"
+              />
+            </div>
           </div>
         </div>
 
@@ -54,7 +64,7 @@ const ProcessPage = () => {
             <img
               src={photo}
               alt="Picture of RIT"
-              className="mr-[2rem] mb-[2rem] md:mt-0 self-center md:self-end md:w-[400px] w-1/4"
+              className="md:mr-[2rem] mb-[2rem] md:mt-0 self-center md:self-end md:w-[400px] w-full md:w-1/4 max-h-[300px]"
             />
             <p className="">
               Lorem ipsum dolor sit amet consectetur. Eget pharetra accumsan
@@ -93,7 +103,7 @@ const ProcessPage = () => {
             <img
               src={photo}
               alt="Picture of RIT"
-              className="md:mt-0 self-center md:self-end md:w-[400px] w-1/4"
+              className="md:mt-0 self-center md:self-end md:w-[400px] w-full md:w-1/4 max-h-[300px]"
             />
           </div>
         </div>
@@ -114,21 +124,27 @@ const ProcessPage = () => {
             malesuada pretium vulputate faucibus leo pellentesque. Curabitur
             commodo nibh egestas pretium adipiscing rhoncus.
           </p>
-          <div className="mt-[2rem] flex flex-row flex-wrap w-full justify-center">
+          <div className="mt-2 flex flex-row flex-wrap w-[85%] m-auto justify-center">
             {characters.map((c) => {
               return (
                 <Dialog>
-                  <DialogTrigger className="hover:brightness-125">
+                  <DialogTrigger className="relative w-full sm:w-1/2 lg:w-1/3 m-auto">
+                    <div className="absolute w-[84.5%] h-[84.5%] m-2 md:m-10 bg-[#0007] opacity-0 hover:opacity-100 rounded-lg">
+                      <h3 className="text-white text-2xl absolute top-[50%] text-center w-full">
+                        Learn about {c.name}'s development
+                      </h3>
+                    </div>
                     <img
                       src={c.imgUrl}
                       alt={c.name}
-                      className="w-60 h-40 lg:w-110 lg:h-90 m-5"
+                      className="w-full p-2 md:p-10 rounded-[10%]"
                     />
                   </DialogTrigger>
                   <DialogContent className="lg:max-w-[60%]">
                     <DialogHeader className="text-left">
                       <DialogTitle className="text-4xl painterz mb-4">
-                        {c.name}{c.occupation ? ` - ${c.occupation}` : ""}
+                        {c.name}
+                        {c.occupation ? ` - ${c.occupation}` : ""}
                       </DialogTitle>
                     </DialogHeader>
                     <div className="max-h-[70vh] overflow-auto flex flex-col items-center text-center">
@@ -155,16 +171,20 @@ const ProcessPage = () => {
         <div className="flex flex-row flex-wrap w-full justify-center">
           <Tabs defaultValue="0">
             <TabsList>
-              {tracks.map(t => {
-                return <TabsTrigger value={tracks.indexOf(t).toString()}>
-                  {t.name}
-                </TabsTrigger>
+              {tracks.map((t) => {
+                return (
+                  <TabsTrigger value={tracks.indexOf(t).toString()}>
+                    {t.name}
+                  </TabsTrigger>
+                );
               })}
             </TabsList>
-            {tracks.map(t => {
-              return <TabsContent value={tracks.indexOf(t).toString()}>
-                {t.devDescription}
-              </TabsContent>
+            {tracks.map((t) => {
+              return (
+                <TabsContent value={tracks.indexOf(t).toString()}>
+                  {t.devDescription}
+                </TabsContent>
+              );
             })}
           </Tabs>
         </div>
