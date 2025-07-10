495f68e36101d138f5886f9e27d24f2d91d3c3c9
AazeemVS <av7304@rit.edu>
2025-07-10 10:08:35 -0400

    refactored the about us page
    I moved reusable code into components and significantly cut down the length of code in the about us page.tsx file while maintaining the same output.

--- src/components/about-us/team-tab.tsx
+++ src/components/about-us/team-tab.tsx
@@ -10,7 +10,7 @@ type TeamTabProps = {
 // This component holds all the tabs for a team
 export const TeamTab = ({ teamData, customTitle }: TeamTabProps) => {
   return (
-    <div>
+    <div className="bg-[url(images/card-background-transparent.png)] bg-no-repeat bg-cover pb-[2rem]">
       <h2 className="text-4xl m-1 mx-0 painterz">
         {teamData.length > 0 ? customTitle || teamData[0][0] : "Loading..."}
       </h2>
