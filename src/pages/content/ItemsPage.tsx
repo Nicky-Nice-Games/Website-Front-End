4184dedfc19dd03d2cc5bf65e9f0e19fd2a7f917
That-Bot-Tess <90156876+That-Bot-Tess@users.noreply.github.com>
2025-07-11 11:17:13 -0400

    fixed close icon position

--- src/pages/content/ItemsPage.tsx
+++ src/pages/content/ItemsPage.tsx
@@ -109,9 +109,7 @@ const ItemsPage = () => {
                     initial={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
                     exit={{ opacity: 0, transition: { duration: 0.05 } }}
-
-                    className="flex relative bottom-5 left-22 md:left-24 items-center justify-center rounded-full h-6"
-
+                    className="self-end rounded-full h-6"
                     onClick={() => setActive(null)}
                   >
                     <CloseIcon /> {/* close icon pop up*/}
