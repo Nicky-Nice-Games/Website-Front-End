035b1d2148ffe96a41c6c6b85b352c6a80f4809e
That-Bot-Tess <90156876+That-Bot-Tess@users.noreply.github.com>
2025-07-11 10:22:57 -0400

    size edits

--- src/pages/content/CharactersPage.tsx
+++ src/pages/content/CharactersPage.tsx
@@ -94,7 +94,7 @@ const CharactersPage = () => {
           <CarouselPrevious className="w-8" />
           <CarouselContent className="m-auto py-15 xl:py-22 flex flex-row content-center items-center justify-between">
             {characters.map((character, index) => (
-              <CarouselItem className="md:basis-1/3 p-auto">
+              <CarouselItem className="md:basis-1/3 pl-0 p-auto">
                 <Dialog>
                   {/* Clickable carousel picture*/}
                   <DialogTrigger className="cursor-pointer hover:scale-105 w-full">
