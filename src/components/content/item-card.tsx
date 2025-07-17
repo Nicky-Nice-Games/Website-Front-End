a5e93c7655767f2dfcdec28ed64e806a3ffaa09f
That-Bot-Tess <90156876+That-Bot-Tess@users.noreply.github.com>
2025-07-17 13:50:39 -0400

    small tweaks

--- src/components/content/item-card.tsx
+++ src/components/content/item-card.tsx
@@ -61,7 +61,7 @@ export const ItemCard = memo(function ItemCard({
             setIsActive(true);
             setActiveItem(items[0]);
           }}
-          className="cursor-pointer rounded-full h-60 w-60 xl:h-80 xl:w-80 overflow-hidden"
+          className="cursor-pointer rounded-2xl h-60 w-60 xl:h-80 xl:w-80 overflow-hidden"
         >
           <motion.div layoutId={`image-${category}-${id}`}>
             <div className="h-full w-full flex p-[1rem] hover:scale-105 transition-transform duration-200">
@@ -74,7 +74,6 @@ export const ItemCard = memo(function ItemCard({
           </motion.div>
         </motion.div>
       )}
-
 
       {/* Popup Overlay */}
       <AnimatePresence>
