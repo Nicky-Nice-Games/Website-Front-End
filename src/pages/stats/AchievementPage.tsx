89ac1397b15aaa98b1d66aea76c979b142d43871
AazeemVS <av7304@rit.edu>
2025-07-18 14:01:50 -0400

    updated achievements page

--- src/pages/stats/AchievementPage.tsx
+++ src/pages/stats/AchievementPage.tsx
@@ -3,6 +3,9 @@ import { sumNumberListObject } from "@/utils";
 import { Card, CardContent } from "@/components/ui/card";
 import Pfp from "@/components/stats/pfp";
 import { achievementData } from "@/data/achievements";
+import { motion } from "framer-motion";
+
+const AnimatedCard = motion(Card);
 
 //#region Helper Functions
 const getTotalItemsUsed = (playerData: any): number => {
@@ -76,8 +79,54 @@ export const AchievementsPage = ({
     <div className="bg-[url(/ggk/images/items-background.png)] min-h-screen text-white p-4 md:p-8">
       {/* Main container - matches InfoPage width */}
       <div className="w-full max-w-[1800px] mx-auto pr-8 pl-8">
-        <Card className="bg-black bg-opacity-90 border-2 border-orange-500 w-full mx-auto rounded-lg shadow-lg shadow-orange-500/50">
-          <div className="bg-black m-0 p-0">Achievements</div>
+        <AnimatedCard
+          className="w-full mx-auto rounded-lg border-2 border-orange-500 shadow-lg shadow-orange-500/50"
+          initial={{
+            background:
+              "linear-gradient(45deg, #121212 0%, #2b2b2b 50%, #F76902 100%)",
+            backgroundSize: "200% 200%",
+          }}
+          animate={{
+            background: [
+              "linear-gradient(45deg, #121212 0%, #2b2b2b 50%, #F76902 100%)",
+              "linear-gradient(135deg, #121212 0%, #2b2b2b 50%, #F76902 100%)",
+              "linear-gradient(225deg, #121212 0%, #2b2b2b 50%, #F76902 100%)",
+              "linear-gradient(315deg, #121212 0%, #2b2b2b 50%, #F76902 100%)",
+              "linear-gradient(45deg, #121212 0%, #2b2b2b 50%, #F76902 100%)",
+            ],
+            backgroundPosition: [
+              "0% 0%",
+              "100% 0%",
+              "100% 100%",
+              "0% 100%",
+              "0% 0%",
+            ],
+          }}
+          transition={{
+            duration: 20,
+            ease: "linear",
+            repeat: Infinity,
+          }}
+        >
+          <div className="inline-flex m-0 p-6 rounded-t-lg text-white place-items-center text-3xl gap-4 ">
+            <span>
+              <svg
+                width="32"
+                height="32"
+                viewBox="0 0 15 15"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M0.877014 7.49988C0.877014 3.84219 3.84216 0.877045 7.49985 0.877045C11.1575 0.877045 14.1227 3.84219 14.1227 7.49988C14.1227 11.1575 11.1575 14.1227 7.49985 14.1227C3.84216 14.1227 0.877014 11.1575 0.877014 7.49988ZM7.49985 1.82704C4.36683 1.82704 1.82701 4.36686 1.82701 7.49988C1.82701 8.97196 2.38774 10.3131 3.30727 11.3213C4.19074 9.94119 5.73818 9.02499 7.50023 9.02499C9.26206 9.02499 10.8093 9.94097 11.6929 11.3208C12.6121 10.3127 13.1727 8.97172 13.1727 7.49988C13.1727 4.36686 10.6328 1.82704 7.49985 1.82704ZM10.9818 11.9787C10.2839 10.7795 8.9857 9.97499 7.50023 9.97499C6.01458 9.97499 4.71624 10.7797 4.01845 11.9791C4.97952 12.7272 6.18765 13.1727 7.49985 13.1727C8.81227 13.1727 10.0206 12.727 10.9818 11.9787ZM5.14999 6.50487C5.14999 5.207 6.20212 4.15487 7.49999 4.15487C8.79786 4.15487 9.84999 5.207 9.84999 6.50487C9.84999 7.80274 8.79786 8.85487 7.49999 8.85487C6.20212 8.85487 5.14999 7.80274 5.14999 6.50487ZM7.49999 5.10487C6.72679 5.10487 6.09999 5.73167 6.09999 6.50487C6.09999 7.27807 6.72679 7.90487 7.49999 7.90487C8.27319 7.90487 8.89999 7.27807 8.89999 6.50487C8.89999 5.73167 8.27319 5.10487 7.49999 5.10487Z"
+                  fill="currentColor"
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </span>
+            <span> Achievements</span>
+          </div>
 
           {/* Header/nav bar - matches InfoPage */}
           <Card className="h-auto w-full mx-auto rounded-t-lg bg-gradient-to-br from-[#121212] via-[#2b2b2b] to-[#F76902]/70 border-b-2 border-orange-500">
@@ -194,7 +243,7 @@ export const AchievementsPage = ({
               );
             })}
           </div>
-        </Card>
+        </AnimatedCard>
       </div>
     </div>
   );
